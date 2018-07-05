015a453b15e9a5b580f01491631f2782351b816d
William Saballos <saballos.william@gmail.com>
2018-07-05 15:36:09 -0700

    + using a conditional statement to render props when state has been fully set.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,21 @@
 import React from "react";
 function Quiz(props) {
+  const selectedChamp = props.randomChamps[props.randomChamps.length - 1]
   return (
     <div className="container">
-      <img src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${props.randomChamp}.png`} />
+      {selectedChamp ? ( 
+        <div>
+          <img
+            src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${selectedChamp.image.full}`}
+            alt={selectedChamp.name}/>
+          <p>{selectedChamp.skills}</p>
+        </div>
+      ) : (
+        <div>
+          <img src={'http://via.placeholder.com/350x150'} alt='placeholder'/>
+          <p>lorem</p>
+        </div>
+      )}
     </div>
   );
 }
