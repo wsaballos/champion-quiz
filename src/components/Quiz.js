2479a48cd4f3c797093a3ba084a099a5e71760c2
William Saballos <saballos.william@gmail.com>
2018-07-05 19:14:16 -0700

    + added font awesome spinner while state is being added 5 random champs. + mapping through array of spells to generate list items. + usiong champs as props rather than random champs.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,19 +1,37 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 function Quiz(props) {
-  const selectedChamp = props.randomChamps[props.randomChamps.length - 1]
+  const selectedChamp = props.champs[props.champs.length - 1];
   return (
     <div className="container">
-      {selectedChamp ? ( 
+      {props.champs.length === 5 ? (
         <div>
           <img
-            src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${selectedChamp.image.full}`}
-            alt={selectedChamp.name}/>
-          <p>{selectedChamp.skills}</p>
+            src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${
+              selectedChamp.image.full
+            }`}
+            alt={selectedChamp.name}
+          />
+          <ul>
+            {selectedChamp.spells.map(spell => {
+              return (
+                <li key={spell.id}>
+                  <img
+                    src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${
+                      spell.image.full
+                    }`}
+                    alt={spell.name}
+                  />
+                  {spell.name}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ) : (
         <div>
-          <img src={'http://via.placeholder.com/350x150'} alt='placeholder'/>
-          <p>lorem</p>
+          <FontAwesomeIcon icon={faSpinner} size="6x" spin />
         </div>
       )}
     </div>
