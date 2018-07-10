d5487ecf0536b7505a230412963eb3380843300d
William Saballos <saballos.william@gmail.com>
2018-07-10 19:01:45 -0400

    ^ updated ternary that checked for 5 champs to only check for 4 champs before rendering. + added new conts and used those to randomize the image and spell being rendered.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,35 +4,42 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function Quiz(props) {
   const selectedChamp = props.champs[props.champs.length - 1];
+  const randChamps = props.champs;
+  const randomSpells = [0, 1, 2, 3].sort(() => 0.5 - Math.random());
+
   return (
     <div className="container">
-      {props.champs.length === 5 ? (
+      {props.champs.length === 4 ? (
         <div>
           <img
             src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${
               selectedChamp.image.full
-            }`}
+              }`}
             alt={selectedChamp.name}
           />
+          <p>
+            {selectedChamp.name}
+          </p>
           <ul>
-            {selectedChamp.spells.map(spell => (
-              <li key={spell.id}>
-                <img
-                  src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${
-                    spell.image.full
-                  }`}
-                  alt={spell.name}
-                />
-                {spell.name}
-              </li>
-            ))}
+            {selectedChamp.spells
+              .map((spell, i) => (
+                <li key={spell.id}>
+                  <img
+                    src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${
+                      randChamps[randomSpells[i]].spells[randomSpells[i]].image.full
+                      }`}
+                    alt={spell.name}
+                  />
+                  {randChamps[randomSpells[i]].spells[randomSpells[i]].name}
+                </li>
+              ))}
           </ul>
         </div>
       ) : (
-        <div>
+          <div>
             <FontAwesomeIcon icon={faSpinner} size="6x" spin />
           </div>
-      )}
+        )}
     </div>
   );
 }
