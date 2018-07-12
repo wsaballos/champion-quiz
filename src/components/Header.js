0ed2578ff1f8edd25259a68ad5c07a9c6d19ba21
William Saballos <saballos.william@gmail.com>
2018-07-12 10:11:22 -0400

    + added basic styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="container">
-      <h1>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <div className="row">
+          <h1>
 Champion Quiz
 </h1>
-      <p>
+        </div>
+        <div className="row">
+          <p>
 Select a spell associated to the Champion shown
 </p>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Header;
