c305dd7d4fb887905ef9d7676074b077287010c4
William Saballos <saballos.william@gmail.com>
2018-07-11 20:51:50 -0400

    [+] added gameState function which alerts boolean value of checkSpell and runs resetState in the App component [+] added style to list items. [^] added gameState fucntion to onClick and onKeydown [+] added button for randChamp.name and styled it.

--- src/components/quiz_list_items.js
+++ src/components/quiz_list_items.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const QuizListItems = ({ spell, randChamps, checkSpell }) => (
-  <li onClick={() => alert(checkSpell)}>
-    <img
-      className="d-none"
-      src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${
-        randChamps.image.full
-      }`}
-      alt={randChamps.name}
-    />
-    <span>
-      {randChamps.name}
-    </span>
-  </li>
-);
+const QuizListItems = ({ randChamps, generateRandomChamps, checkSpell }) => {
+  const gameState = () => {
+    alert(checkSpell);
+    generateRandomChamps();
+  };
+  return (
+    <li style={{ listStyle: 'none' }}>
+      <div onClick={() => gameState()} onKeyDown={(e) => { if (e.keyCode === 13) { gameState(); } }} role="presentation" tabIndex="-1">
+        <img
+          className="d-none"
+          src={`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${
+            randChamps.image.full
+          }`}
+          alt={randChamps.name}
+        />
+        <span>
+          <button className="btn btn-primary" style={{ margin: 5 }} type="button">
+            {randChamps.name}
+          </button>
+        </span>
+      </div>
+    </li>
+  );
+};
 export default QuizListItems;
